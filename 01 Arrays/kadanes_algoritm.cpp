21d7b593918ae7f8169a9cfd89788c0f679e69c2
Abhishek Jha <abhs.jha@gmail.com>
2021-09-23 00:31:44 +0530

    added kadanes algorithm.cpp

--- 01 Arrays/kadanes_algoritm.cpp
+++ 01 Arrays/kadanes_algoritm.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
 using namespace std;
 
-// Print all subarrays of given array.
-void print_subarrays(int*arr, int n){
-    for(int i=0; i<n; i++){  // starting point of the subarr
-        for(int j=i; j<n; j++){ // ending point of the subarr
-            
-            /* for looping inside these subarrays */
-            for(int k=i; k<=j; k++){
-                cout << arr[k] << ", ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
+int maximum_subarray_sum(int arr[],int n){
+
+	int cs = 0;
+	int largest = 0;
+
+	for(int i=0;i<n;i++){
+		cs = cs + arr[i];
+		if(cs < 0){
+			cs = 0;
+		}
+		largest = max(largest, cs);
+	}
+
+	return largest;
 }
 
+
 int main(){
+	//Array Containing 
+	int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+	int n = sizeof(arr)/sizeof(int);
 
-    int arr[] = {10,20,30,40,50, 60};
-    int n = sizeof(arr)/sizeof(int);
+	cout<< maximum_subarray_sum(arr,n) <<endl;
 
-    print_subarrays(arr, n);
-
-    return 0;
+	return 0;
 }
