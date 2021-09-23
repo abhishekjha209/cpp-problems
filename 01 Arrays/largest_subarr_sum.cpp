d268cdf9c2f127a1f18767003be7da6611b8e1a5
Abhishek Jha <abhs.jha@gmail.com>
2021-09-23 19:48:44 +0530

    updated largest_subarr_sum

--- 01 Arrays/largest_subarr_sum.cpp
+++ 01 Arrays/largest_subarr_sum.cpp
@@ -44,29 +44,29 @@ int largest_subarr_sum3(int*arr, int n){
     // generate prefix arr.
     int prefix[n];
     prefix[0] = arr[0];
-	for(int i=1;i<n;i++){
-		prefix[i] = prefix[i-1] + arr[i];
-	}
+    for(int i=1;i<n;i++){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
 
     int largest_sum = 0;
-	for(int i=0;i<n;i++){
-		for(int j=i; j<n; j++){
-			int subarraySum = i>0 ? prefix[j] - prefix[i-1] : prefix[j]; 
+    for(int i=0;i<n;i++){
+        for(int j=i; j<n; j++){
+            int subarraySum = i>0 ? prefix[j] - prefix[i-1] : prefix[j]; 
 
-			//put a check is subarraySum > largest_sum
-			largest_sum = max(largest_sum,subarraySum);
+            //put a check is subarraySum > largest_sum
+            largest_sum = max(largest_sum,subarraySum);
 
-		}
-	}
+        }
+    }
 
-	return largest_sum;
+    return largest_sum;
 }
 
 
 int main(){
     //Array Containing 
-	int arr[] = {-2,3,4,-1,5,-12,6,1,3};
-	int n = sizeof(arr)/sizeof(int);
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+    int n = sizeof(arr)/sizeof(int);
 
     int largest_sum = largest_subarr_sum2(arr, n);
     cout << "Largest sum: " << largest_sum << endl;
