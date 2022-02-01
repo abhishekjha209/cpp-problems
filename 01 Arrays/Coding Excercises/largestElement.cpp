821f188f25e3e63097a42e91f874ba8034af7660
Abhishek Jha <abhs.jha@gmail.com>
2022-02-01 13:58:00 +0530

    updated file comments and code

--- 01 Arrays/Coding Excercises/largestElement.cpp
+++ 01 Arrays/Coding Excercises/largestElement.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 
-int largestElement(vector<int> arr) {
+int largestElement(vector<int> arr){
     int largest = arr[0];
-    for(int i=0; i<arr.size(); i++){
-        largest = max(largest, arr[i]);
+    for(int i=1; i<arr.size(); i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
     }
     return largest;
 }
 
+int largestElement2(vector<int> arr){
+    return *max_element(arr.begin(), arr.end()); 
+        //returns the iterator to largest element in the vector
+}
 
 int main(){
-    vector<int> arr = {1, -2, 3, 4, 4, -2}; 
-    cout << largestElement(arr);
+    vector<int> arr = {1, -2, 3, 4, 42, -2}; 
+    cout << largestElement2(arr);
     return 0;
 }
--- 01 Arrays/Coding Excercises/lowerBound.cpp
+++ 01 Arrays/Coding Excercises/lowerBound.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Question in brief:
+
+// Given an array of integers "arr"(sorted), and a integer "Val". 
+// Implement a function to find lower bound of Val in "arr".
+// If Val is not present in "arr", than lower bound of a given integer means element just smallar than Val.
+// If Val is less than smallest element in "arr", then return -1 in that case.
+
+
+
+// Function to find the lower bound of the element
 int lowerBound(vector<int> arr, int Val) {
     int l = 0;
     int r = arr.size()-1;
@@ -16,7 +26,7 @@ int lowerBound(vector<int> arr, int Val) {
         }else if(Val < arr[mid]){
             r = mid - 1;
         }else{
-            ans = arr[l];
+            ans = arr[l]; // arr[l] represents lower bound of Val, (before updating pointers for next iteration range)
             l = mid + 1;
         }
         
